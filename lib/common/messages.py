0f8f77d42b58ddddc7c642fe18fcf678d651d26b
Shady H <40376545+shaddih1@users.noreply.github.com>
2021-08-21 23:04:32 -0500

    Messages

---
+++ lib/common/messages.py
@@ -0,0 +1,23 @@
+#! -*- coding: utf8 -*-
+
+import os
+
+def getDir():
+    get_dir = home_dir = os.path.expanduser("~")
+    return ger_dir
+
+get_dir = getDir()
+
+# messages 
+def argumentMessage():
+    message = f'''\n┌──[Phynance]─[{get_dir}]
+└──╼ $'''
+    return message
+
+def header(quiet):
+    if not quiet:
+        print(f'''╔═╗┬ ┬┬ ┬┌┐┌┌─┐┌┐┌┌─┐┌─┐
+╠═╝├─┤└┬┘│││├─┤││││  ├┤ 
+╩  ┴ ┴ ┴ ┘└┘┴ ┴┘└┘└─┘└─┘by Shady
+  Made with <3 | Tools''')
+
